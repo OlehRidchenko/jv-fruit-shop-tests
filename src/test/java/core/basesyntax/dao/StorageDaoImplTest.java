7ac38652368657d902392dff0277e5853ac8a7e5
Oleh Ridchenko <olehridchenko@gmail.com>
2025-07-21 15:15:38 +0300

    + maven checkstyle errors fixed

--- src/test/java/core/basesyntax/dao/StorageDaoImplTest.java
+++ src/test/java/core/basesyntax/dao/StorageDaoImplTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import core.basesyntax.db.Storage;
 import java.util.Map;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
